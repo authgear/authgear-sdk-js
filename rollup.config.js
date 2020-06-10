faed651ad6d9c773273ab51a8266c91fe67cc2ef
Louis Chan <louischan@oursky.com>
2020-06-10 11:43:46 +0800

    Remove unused deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,60 +1,13 @@
-import { readFileSync, writeFile } from "fs";
-import { promisify } from "util";
+import { readFileSync } from "fs";
 import babel from "rollup-plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
 import json from "rollup-plugin-json";
 import resolve from "rollup-plugin-node-resolve";
 import replace from "rollup-plugin-replace";
 
-const ClosureCompiler = require("google-closure-compiler").compiler;
-const tmp = require("tmp");
 const getBuiltins = require("builtins");
 
 const extensions = [".mjs", ".js", ".jsx", ".ts", ".tsx"];
-
-const writeFileAsync = promisify(writeFile);
-
-function compile(flags) {
-  return new Promise((resolve, reject) => {
-    const compiler = new ClosureCompiler(flags);
-    compiler.run((exitCode, stdout, stderr) => {
-      if (exitCode === 0) {
-        resolve(stdout);
-      } else {
-        reject(new Error(stderr));
-      }
-    });
-  });
-}
-
-function closure(flags) {
-  return {
-    name: "plugin-google-closure-compiler",
-    async renderChunk(code) {
-      const inputFile = tmp.fileSync();
-      const tempPath = inputFile.name;
-      flags = Object.assign({}, flags, { js: tempPath });
-      await writeFileAsync(tempPath, code, "utf8");
-      const compiledCode = await compile(flags);
-      inputFile.removeCallback();
-      return { code: compiledCode };
-    },
-  };
-}
-
-const closurePlugin = closure({
-  compilation_level: "SIMPLE",
-  language_in: "ECMASCRIPT5_STRICT",
-  language_out: "ECMASCRIPT5_STRICT",
-  env: "CUSTOM",
-  warning_level: "VERBOSE",
-  apply_input_source_maps: false,
-  use_types_for_optimization: false,
-  process_common_js_modules: false,
-  rewrite_polyfills: false,
-  inject_libraries: false,
-  jscomp_off: "undefinedVars",
-});
 
 const plugins = [
   replace({
