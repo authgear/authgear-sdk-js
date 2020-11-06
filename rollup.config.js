046b16510ab4b7b78848b86f82acd6a82b054d1c
nicolaslee2775 <13070797d@connect.polyu.hk>
2020-11-06 18:30:49 +0800

    Upgrade rollup-plugin-json to 4.1.0 #34

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import { readFileSync } from "fs";
 import babel from "rollup-plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
-import json from "rollup-plugin-json";
+import json from "@rollup/plugin-json";
 import resolve from "rollup-plugin-node-resolve";
 import replace from "rollup-plugin-replace";
 
