e2089165783abdee9cb396bd6610887894eea0fa
nicolaslee2775 <13070797d@connect.polyu.hk>
2020-11-06 18:33:45 +0800

    Upgrade rollup-plugin-node-resolve to 10.0.0 #34

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import { readFileSync } from "fs";
 import babel from "rollup-plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
-import resolve from "rollup-plugin-node-resolve";
+import resolve from "@rollup/plugin-node-resolve";
 import replace from "rollup-plugin-replace";
 
 const getBuiltins = require("builtins");
