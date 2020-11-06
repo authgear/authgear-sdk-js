9a6a210269212bd3c4a428182c086f3496d3f2c3
nicolaslee2775 <13070797d@connect.polyu.hk>
2020-11-06 18:26:08 +0800

    Upgrade rollup-plugin-commonjs to 16.0.0 #34

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import { readFileSync } from "fs";
 import babel from "rollup-plugin-babel";
-import commonjs from "rollup-plugin-commonjs";
+import commonjs from "@rollup/plugin-commonjs";
 import json from "rollup-plugin-json";
 import resolve from "rollup-plugin-node-resolve";
 import replace from "rollup-plugin-replace";
