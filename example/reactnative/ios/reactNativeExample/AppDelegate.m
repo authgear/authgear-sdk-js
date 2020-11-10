d84bd2602cf4967acb5811fc4c435c375c38d551
axolotl0212 <allenl0212@gmail.com>
2020-11-10 13:17:34 +0800

    Add deeplink setup for Authgear SDK

--- example/reactnative/ios/reactNativeExample/AppDelegate.m
+++ example/reactnative/ios/reactNativeExample/AppDelegate.m
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <AGAuthgearReactNative.h>
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -44,6 +45,13 @@ static void InitializeFlipper(UIApplication *application) {
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
+}
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [AGAuthgearReactNative application:application openURL:url options:options];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
