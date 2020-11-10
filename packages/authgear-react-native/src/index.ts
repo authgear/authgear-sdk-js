52e2e623f6e8716b9c8a4eb0ba3cd03a5e88d3f4
axolotl0212 <allenl0212@gmail.com>
2020-11-10 13:37:23 +0800

    Update react native async storage dependency

--- packages/authgear-react-native/src/index.ts
+++ packages/authgear-react-native/src/index.ts
@@ -1,5 +1,5 @@
 /* global fetch, Request */
-import AsyncStorage from "@react-native-community/async-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import {
   BaseAPIClient,
   ContainerOptions,
