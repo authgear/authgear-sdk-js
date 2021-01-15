aff4754c4eba93f8e75d444c05ff1ee458441575
Carmen <carmen@oursky.com>
2021-01-15 19:10:50 +1100

    Implement WeChat login in ios example

--- example/reactnative/ios/reactNativeExample/AppDelegate.h
+++ example/reactnative/ios/reactNativeExample/AppDelegate.h
@@ -1,7 +1,8 @@
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
+#import "WXApi.h"
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, WXApiDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
 
