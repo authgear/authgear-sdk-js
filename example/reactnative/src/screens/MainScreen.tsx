954ae60b94a5bb65b001274ffcc02ebd56668e5c
Louis Chan <louischan@oursky.com>
2021-03-31 11:17:40 +0800

    Showcase how to handle biometric error

--- example/reactnative/src/screens/MainScreen.tsx
+++ example/reactnative/src/screens/MainScreen.tsx
@@ -18,6 +18,12 @@ import authgear, {
   BaseAPIClient,
   SessionStateChangeReason,
   UserInfo,
+  isBiometricCancel,
+  isBiometricPrivateKeyNotFoundError,
+  isBiometricNotSupportedOrPermissionDenied,
+  isBiometricNoEnrollment,
+  isBiometricNoPasscode,
+  isBiometricLockout,
 } from '@authgear/react-native';
 
 const styles = StyleSheet.create({
@@ -147,8 +153,59 @@ const HomeScreen: React.FC = () => {
     delete json['sourceURL'];
     json['constructor.name'] = e?.constructor?.name;
     json['message'] = e.message;
-    const message = JSON.stringify(json);
     const title = 'Error';
+    let message = JSON.stringify(json);
+
+    if (isBiometricCancel(e)) {
+      // Cancel is not an error actually.
+      return;
+    }
+
+    if (isBiometricPrivateKeyNotFoundError(e)) {
+      message = Platform.select({
+        android:
+          'Your biometric info has changed. For security reason, you have to set up biometric authentication again.',
+        ios:
+          'Your Touch ID or Face ID has changed. For security reason, you have to set up biometric authentication again.',
+        default: message,
+      });
+    }
+
+    if (isBiometricNoEnrollment(e)) {
+      message = Platform.select({
+        android:
+          'You have not set up biometric yet. Please set up your fingerprint or face',
+        ios:
+          'You do not have Face ID or Touch ID set up yet. Please set it up first',
+        default: message,
+      });
+    }
+
+    if (isBiometricNotSupportedOrPermissionDenied(e)) {
+      message = Platform.select({
+        android:
+          'Your device does not support biometric. The developer should have checked this and not letting you to see feature that requires biometric',
+        ios:
+          'If the developer should performed checking, then it is likely that you have denied the permission of Face ID. Please enable it in Settings',
+        default: message,
+      });
+    }
+
+    if (isBiometricNoPasscode(e)) {
+      message = Platform.select({
+        android:
+          'You device does not have credential set up. Please set up either a PIN, a pattern or a password',
+        ios:
+          'You device does not have passcode set up. Please set up a passcode',
+        default: message,
+      });
+    }
+
+    if (isBiometricLockout(e)) {
+      message =
+        'The biometric is locked out due to too many failed attempts. The developer should handle this error by using normal authentication as a fallback. So normally you should not see this error';
+    }
+
     Alert.alert(title, message);
   }, []);
 
