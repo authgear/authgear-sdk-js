d482f333aa393792fe08ec13523b1ef6b6ab678e
Louis Chan <louischan@oursky.com>
2021-07-07 11:59:40 +0800

    Document why padding is not needed

--- packages/authgear-core/src/base64.ts
+++ packages/authgear-core/src/base64.ts
@@ -14,5 +14,8 @@ export function _base64URLEncode(arrayBuffer: ArrayBuffer): string {
  * @internal
  */
 export function _base64URLDecode(s: string): ArrayBuffer {
+  // base64-arraybuffer ignores padding actually.
+  // https://github.com/niklasvh/base64-arraybuffer/blob/master/lib/base64-arraybuffer.js#L44
+  // so we need not bother with adding padding to the encoded string.
   return decode(s.replace(/-/g, "+").replace(/_/g, "/"));
 }
