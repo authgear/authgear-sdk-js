66509de4120ef2a6df357d94b8951112b81d5c0c
Louis Chan <louischan@oursky.com>
2021-08-20 14:13:44 +0800

    Redirect / to /docs/

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,29 +1,6 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
-}
+import React from "react";
+import { Redirect } from "@docusaurus/router";
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-    </Layout>
-  );
+  return <Redirect to="docs/" />;
 }
