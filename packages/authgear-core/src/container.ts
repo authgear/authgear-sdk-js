b5440cfba0b1f7998dfefdcaa6a6552d0bd7e036
Louis Chan <louischan@oursky.com>
2022-03-01 15:19:24 +0800

    Bump deps to compatible version

--- packages/authgear-core/src/container.ts
+++ packages/authgear-core/src/container.ts
@@ -265,7 +265,7 @@ export class _BaseContainer<T extends _BaseAPIClient> {
       // When the error is `invalid_grant`, the refresh token is no longer valid.
       // Clear the session in this case.
       // https://tools.ietf.org/html/rfc6749#section-5.2
-      if (error.error === "invalid_grant") {
+      if (error != null && (error as any).error === "invalid_grant") {
         await this._clearSession("INVALID");
         return;
       }
