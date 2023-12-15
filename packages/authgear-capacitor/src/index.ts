4a92b500ee0ee3a84908ae9dbfe3ecdafe459d3a
Louis Chan <louischan@oursky.com>
2023-12-15 15:11:42 +0800

    Run npm run format

--- packages/authgear-capacitor/src/index.ts
+++ packages/authgear-capacitor/src/index.ts
@@ -1,12 +1,12 @@
-import { registerPlugin } from '@capacitor/core';
+import { registerPlugin } from "@capacitor/core";
 
-import type { AuthgearPlugin } from './definitions';
+import type { AuthgearPlugin } from "./definitions";
 
-const Authgear = registerPlugin<AuthgearPlugin>('Authgear', {
+const Authgear = registerPlugin<AuthgearPlugin>("Authgear", {
   // This file is generated, thus the following dynamic import statement.
   // @ts-expect-error
-  web: () => import('./web').then(m => new m.AuthgearWeb()),
+  web: () => import("./web").then((m) => new m.AuthgearWeb()),
 });
 
-export * from './definitions';
+export * from "./definitions";
 export { Authgear };
--- packages/authgear-capacitor/src/web.ts
+++ packages/authgear-capacitor/src/web.ts
@@ -1,10 +1,10 @@
-import { WebPlugin } from '@capacitor/core';
+import { WebPlugin } from "@capacitor/core";
 
-import type { AuthgearPlugin } from './definitions';
+import type { AuthgearPlugin } from "./definitions";
 
 export class AuthgearWeb extends WebPlugin implements AuthgearPlugin {
   async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
+    console.log("ECHO", options);
     return options;
   }
 }
