0ae1b47963b1403a947106bea910bdf6dfe24122
Louis Chan <louischan@oursky.com>
2023-12-15 15:10:31 +0800

    Make npm run typecheck exit with 0 again

--- packages/authgear-capacitor/src/index.ts
+++ packages/authgear-capacitor/src/index.ts
@@ -3,6 +3,8 @@ import { registerPlugin } from '@capacitor/core';
 import type { AuthgearPlugin } from './definitions';
 
 const Authgear = registerPlugin<AuthgearPlugin>('Authgear', {
+  // This file is generated, thus the following dynamic import statement.
+  // @ts-expect-error
   web: () => import('./web').then(m => new m.AuthgearWeb()),
 });
 
