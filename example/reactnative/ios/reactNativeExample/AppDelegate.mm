8bb6bfa5004386db113cac93cdb670006d29a234
Louis Chan <louischan@oursky.com>
2025-02-04 22:16:32 +0800

    Upgrade react-native from 0.73.11 to 0.74.7
    See https://react-native-community.github.io/upgrade-helper/?from=0.73.11&to=0.74.7

--- example/reactnative/ios/reactNativeExample/AppDelegate.mm
+++ example/reactnative/ios/reactNativeExample/AppDelegate.mm
@@ -28,10 +28,10 @@ NSString* const WechatAuthErrorDomain = @"com.authgear.example.reactnative.wecha
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-  return [self getBundleURL];
+  return [self bundleURL];
 }
 
-- (NSURL *)getBundleURL
+- (NSURL *)bundleURL
 {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
