a20e3c1ccf85d5c48496d427c43dfe163884d3b3
Louis Chan <louischan@oursky.com>
2025-08-21 12:58:34 +0800

    Suppress an incorrect type error

--- example/reactnative/src/App.tsx
+++ example/reactnative/src/App.tsx
@@ -12,6 +12,7 @@ const App: React.FC = () => {
   return (
     <>
       <StatusBar barStyle="light-content" />
+      {/* @ts-expect-error */}
       <View style={safeAreaStyle}>
         <MainScreen />
       </View>
