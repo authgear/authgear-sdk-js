8562325749836a9e9c3aaa41b54aad583934bef3
Tung Wu <tung@oursky.com>
2025-10-03 15:51:58 +0800

    Upgrade packages in reactweb example

--- example/reactweb/src/index.tsx
+++ example/reactweb/src/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
