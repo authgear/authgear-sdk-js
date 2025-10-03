fcd8a66aad38ddea8f85394ba93ab3264869cea2
Tung Wu <tung@oursky.com>
2025-10-03 15:45:51 +0800

    Add eslint to reactweb example

---
+++ example/reactweb/.eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-hooks"],
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "error",
+  },
+  ignorePatterns: ["dist/"],
+};
